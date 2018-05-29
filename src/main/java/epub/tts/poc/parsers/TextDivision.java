3106e771aa58665b702df8239c8b21557ee911b2
stesk <ssp@nota.nu>
2018-05-29 15:40:50 +0200

    Include division node in TextDivision

--- src/main/java/epub/tts/poc/parsers/TextDivision.java
+++ src/main/java/epub/tts/poc/parsers/TextDivision.java
@@ -2,6 +2,14 @@ package epub.tts.poc.parsers;
 
 import java.util.LinkedList;
 
+import net.sf.saxon.s9api.XdmNode;
+
 public class TextDivision extends LinkedList<TextBlock> {
+	
+	private XdmNode contentNode;
+	
+	public TextDivision(XdmNode contentNode) {
+		this.contentNode = contentNode;
+	}
 
 }
